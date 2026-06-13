f223d0d9eb0e614b34c4b9105b5da00623f3f5cc
Naty,moniss <natalymonique18@gmail.com>
2026-06-13 16:39:02 -0300

    fix: add include directory with header files

---
+++ 01_analisador_estatistico/include/estatisticas.h
@@ -0,0 +1,9 @@
+#ifndef ESTATISTICAS_H
+#define ESTATISTICAS_H
+
+// Declaração das funções estatísticas
+float calcular_media(float valores[], int tamanho);
+float buscar_maximo(float valores[], int tamanho);
+float buscar_minimo(float valores[], int tamanho);
+
+#endif
